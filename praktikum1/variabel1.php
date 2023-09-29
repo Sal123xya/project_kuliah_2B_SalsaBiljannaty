a61bf948d5abbbb4bf36c291481e769158e04525
salsa <salsa.biljannaty@gmail.com>
2023-09-29 13:27:18 +0700

    initial files

---
+++ praktikum1/variabel1.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<head>
+    <title>Contoh Skrip PHP</title>
+</head>
+<body>
+    <?php 
+        $nama = "Salsa Biljannaty";
+        echo "$nama berkata \"Katakan namamu!\"";
+    ?>
+</body>
+</html>
---
+++ praktikum2/break.php
@@ -0,0 +1,16 @@
+<html>
+    <body>
+        <?php
+            //melakukan break pada $i == 2
+            for($i=0; $a < 5; $i++)
+            {
+                if($i == 2) 
+                {
+                    break;
+                }
+                echo("nilai i : $i <br>");
+            }
+            echo("Loop selesai");
+        ?>
+    </body>
+</html>
