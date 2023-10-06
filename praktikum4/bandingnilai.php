74c1c9efea1b51a9398fa1ca6e896ca0aee173ff
salsa <salsa.biljannaty@gmail.com>
2023-10-07 01:32:42 +0700

    tambahan program

---
+++ praktikum4/bandingnilai.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <title>Contoh Form dengan POST</title>
+    </head>
+    <body>
+        <form action="pro_post_nilai.php" method="post">
+            Bil I : <input type="text" name="bil1">
+            <BR>
+            Bil II : <input type="text" name="bil2">
+            <BR>
+            <input type="submit" value="Bandingkan">
+        </form>
+    </body>
+</html>
