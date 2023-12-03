3a45f55939fba31ab360cda521617aab4984a29b
salsa <salsa.biljannaty@gmail.com>
2023-12-03 19:47:57 +0700

    project

--- projectbutik/proses/proses_login.php
+++ projectbutik/proses/proses_login.php
@@ -4,7 +4,7 @@
     $password = (isset($_POST['password'])) ? md5(htmlentities($_POST['password'])) : "" ;
 
     if(!empty($_POST['submit_validate'])){
-        $query = mysqli_query($conn, "SELECT * FROM tb_user WHERE username = '$usename' && password = '$password'");
+        $query = mysqli_query($conn, "SELECT * FROM tb_user WHERE username = '$username' && password = '$password'");
         $hasil = mysqli_fetch_array($query);
         if($hasil){
             header('location:../home');
